3c9bbe71a21efdbec83724772c0e1f659059e899
Brennon Nuckols <brennuck@gmail.com>
2020-04-22 14:02:43 -0600

    Private route built

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from './Components/Login';
+import PrivateRoute from './Components/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
           <Route path="login" component={Login} />
           <Route component={Login} />
         </Switch>
